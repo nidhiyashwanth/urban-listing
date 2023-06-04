f9c30d867eb27cc7d12d34873044a5d53abf6684
Nidhi Yashwanth <nidhiyashwanth007@gmail.com>
2023-06-04 17:38:56 +0530

    create header component

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import Offers from "./pages/Offers"
 import ForgotPassword from "./pages/ForgotPassword"
 import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
+import Header from "./components/Header";
 function App() {
   return (
     <>
       <Router>
+        <Header/>
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/profile" element={<Profile/>}/>
