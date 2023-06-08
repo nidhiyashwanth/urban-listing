8f8cacbccc82415df96a4f29cbbc29de20e984f5
Nidhi Yashwanth <nidhiyashwanth007@gmail.com>
2023-06-08 21:49:36 +0530

    fix bug with border bottom in header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -29,9 +29,9 @@ export default function Header() {
         </div>
         <div>
           <ul className='flex space-x-10'>
-            <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/") && "text-black border-b-red-500"}`} onClick={() => navigate("/")}>Home</li>
-            <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/offers") && "text-black border-b-red-500"}`} onClick={() => navigate("/offers")}>Offers</li>
-            <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${(pathMatchRoute("/sign-in") || pathMatchRoute("/profile")) && "text-black border-b-red-500"}`} onClick={() => navigate("/profile")}>{pageState}</li>
+            <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] ${pathMatchRoute("/") ? "text-black border-b-red-500" : "border-b-transparent"}`} onClick={() => navigate("/")}>Home</li>
+            <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] ${pathMatchRoute("/offers") ? "text-black border-b-red-500" : "border-b-transparent"}`} onClick={() => navigate("/offers")}>Offers</li>
+            <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] ${pathMatchRoute("/sign-in") || pathMatchRoute("/profile") ? "text-black border-b-red-500" : "border-b-transparent"}`} onClick={() => navigate("/profile")}>{pageState}</li>
           </ul>
         </div>
       </header>
