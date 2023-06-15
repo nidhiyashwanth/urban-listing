8dfaea7fe564f369eeec599dfb52b1640b2aa709
Nidhi Yashwanth <nidhiyashwanth007@gmail.com>
2023-06-15 21:41:54 +0530

    create geo location element

--- src/App.js
+++ src/App.js
@@ -23,10 +23,12 @@ function App() {
             <Route path="/profile" element={<Profile/>}/>
           </Route>
           <Route path="/offers" element={<Offers/>}/>
-          <Route path="/create-listing" element={<CreateListing/>}/>
           <Route path="/forgot-password" element={<ForgotPassword/>}/>
           <Route path="/sign-in" element={<SignIn/>}/>
           <Route path="/sign-up" element={<SignUp/>}/>
+          <Route path="create-listing" element={<PrivateRoute/>}>
+            <Route path="/create-listing" element={<CreateListing/>}/>
+          </Route>
         </Routes>
       </Router>
       <ToastContainer
--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,8 +1,9 @@
-import { type } from '@testing-library/user-event/dist/type'
 import React, { useState } from 'react'
 
 
 export default function CreateListing() {
+  const [geolocationEnabled, setGeolocationEnabled] = useState(true)
+
   const [formData, setFormData] = useState({
     type: "rent",
     name: "",
@@ -15,22 +16,50 @@ export default function CreateListing() {
     offer: false,
     regularPrice: 0,
     discountedPrice: 0,
+    latitude: 0,
+    longitude: 0
   });
 
-  const { type, name, bedrooms, bathrooms, parking, furnished, address, description, offer, regularPrice, discountedPrice } = formData;
-  function onChange() {
-
+  const { type, name, bedrooms, bathrooms, parking, furnished, address, description, offer, regularPrice, discountedPrice, latitude, longitude } = formData;
+  
+  function onChange(e) {
+    let boolean = null;
+    if (e.target.value === "true") {
+      boolean = true;
+    }
+    if (e.target.value === "false") {
+      boolean = false;
+    }
+    // Files
+    if (e.target.files) {
+      setFormData((prevState) => ({
+        ...prevState,
+        images: e.target.files,
+      }));
+    }
+    // Text/Boolean/Number
+    if (!e.target.files) {
+      setFormData((prevState) => ({
+        ...prevState,
+        [e.target.id]: boolean ?? e.target.value,
+      }));
+    }
   }
+  function onSubmit(e){
+    e.preventDefault();
+    
+  }
+
   return (
     <main className=' max-w-md px-2 mx-auto  '>
       <h1 className='text-3xl text-center mt-6 font-bold '>Create a Listing</h1>
-      <form >
+      <form onSubmit={onSubmit} >
         <p className=' text-lg mt-6 font-semibold  '>Sell / Rent</p>
         <div className='flex '>
           <button type='button' id='type' value="sale" onClick={onChange} className={`mr-3 px-7 py-3 font-medium text-sm uppercase shadow-md rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition delay-150 ease-in-out w-full ${type === "rent" ? "bg-white text-black" : "bg-slate-600 text-white"}`}>
             sell
           </button>
-          <button type='button' id='type' value="sale" onClick={onChange} className={`ml-3 px-7 py-3 font-medium text-sm uppercase shadow-md rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition delay-150 ease-in-out w-full ${type === "sale" ? "bg-white text-black" : "bg-slate-600 text-white"}`}>
+          <button type='button' id='type' value="rent" onClick={onChange} className={`ml-3 px-7 py-3 font-medium text-sm uppercase shadow-md rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition delay-150 ease-in-out w-full ${type === "sale" ? "bg-white text-black" : "bg-slate-600 text-white"}`}>
             rent
           </button>
         </div>
@@ -72,6 +101,19 @@ export default function CreateListing() {
 
         <p className='text-lg mt-6 font-semibold'> Address</p>
         <textarea type="text" id='address' value={address} onChange={onChange} placeholder='Address' required className='w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 mb-6  ' />
+        {!geolocationEnabled && (
+          <div className="flex space-x-6 justify-start mb-6">
+          <div className="">
+            <p className='text-lg font-semibold '>Latitude</p>
+            <input type="number" id='latitude' value={latitude} onChange={onChange}  required min="-90" max="90" className=' w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:bg-white focus:text-gray-700 focus:border-slate-600 text-center ' />
+            </div>
+            
+            <div className="">
+            <p className='text-lg font-semibold '>Longitude</p>
+            <input type="number" id='longitude' value={longitude} onChange={onChange} min="-180" max="180"  required className=' w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:bg-white focus:text-gray-700 focus:border-slate-600 text-center ' />
+          </div>
+          </div>
+        )}
         
         <p className='text-lg font-semibold'> Description</p>
         <textarea type="text" id='description' value={description} onChange={onChange} placeholder='Description' required className='w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 mb-6'/>
