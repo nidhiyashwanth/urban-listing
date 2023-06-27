f7e561c04226348cd7b514f72d01b01f42e654cb
Nidhi Yashwanth <nidhiyashwanth007@gmail.com>
2023-06-27 12:50:13 +0530

    complete landlord contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,11 @@
-import { doc, getDoc } from 'firebase/firestore'
-import React, { useEffect } from 'react'
+import { doc, getDoc } from "firebase/firestore";
+import React, { useEffect, useState } from 'react'
 import { toast } from 'react-toastify'
+import {db} from '../firebase'
 
 export default function Contact({userRef, listing}) {
-    const [Landlord, setLandlord] = useState(null)
+    const [landlord, setLandlord] = useState(null);
+    const [message, setMessage] = useState("");
     useEffect(()=>{
         async function getLandlord(){
             const docRef = doc(db, "users", userRef);
@@ -16,8 +18,24 @@ export default function Contact({userRef, listing}) {
             }
         }
         getLandlord()
-    }, [])
+    }, [userRef])
+
+    function onChange(e){
+        setMessage(e.target.value)
+    }
   return (
-    <div>{Landlord}</div>
+    <>
+        {landlord !== null && (
+            <div className="flex flex-col w-full">
+                <p>Contact {landlord.name} for the {listing.name.toLowerCase()}</p>
+                <div className="mt-3 mb-6">
+                    <textarea name="message" id="message" rows="2" value={message} onChange={onChange} className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 "></textarea>
+                </div>
+                <a href={`mailto:${landlord.email}?Subject=${listing.name} & body=${message}`}>
+                    <button className="px-7 py-3 bg-blue-600 text-white rounded text-sm uppercase shadow-md  focus:bg-blue-700 focus:shadow-lg active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full text-center mb-6 " type="button">Send Message</button>
+                </a>
+            </div>
+        )}
+    </>
   )
 }
--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -58,7 +58,7 @@ export default function Listing() {
                 <p className='fixed top-[23%] right-[5%] font-semibold border-2 text-gray-600 border-gray-200 rounded-md bg-white z-10 p-2 '>Link Copied!</p>
             )}
             <div className=" m-4 flex flex-col md:flex-row max-w-6xl lg:mx-auto p-4 rounded-lg shadow-lg bg-white  lg:space-x-5 ">
-                <div className="w-full h-[200px] lg-[400px]  ">
+                <div className="w-full">
                     <p className=' text-2xl font-bold mb-3 text-blue-900'>
                         {listing.name} - ${listing.offer ? listing.discountedPrice.toString()
                             .replace(/\B(?=(\d{3})+(?!\d))/g, ",") : listing.regularPrice.toString()
@@ -84,7 +84,7 @@ export default function Listing() {
                         <span className=' font-semibold'>Description - </span>
                         {listing.description}
                     </p>
-                    <ul className='flex items-center space-x-2 lg:space-x-10 text-sm font-semibold'>
+                    <ul className='flex items-center space-x-2 lg:space-x-10 text-sm font-semibold mb-6'>
                         <li className=' flex items-center whitespace-nowrap'> <FaBed className='text-lg mr-1'/>
                             {+listing.bedrooms > 1 ? `${listing.bedrooms} Beds` : "1 Bed"}
                         </li>
